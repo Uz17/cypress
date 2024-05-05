6ed4b86cb4cfc37a672eb0cd0decf896b3398191
Uzaiz Sumit <uzaiz.sumit17@gmail.com>
2024-05-06 01:22:17 +0600

    add user

--- cypress/e2e/uzi-custom/practice.cy.js
+++ cypress/e2e/uzi-custom/practice.cy.js
@@ -4,5 +4,16 @@ describe('MyFirstTest', () => {
         cy.get("input[placeholder = 'Username']").type('Admin')
         cy.get("input[placeholder = 'Password']").type('admin123')
         cy.get("button[type = 'submit']").click()
+        cy.get("ul[class*='oxd-main-menu'] li a:contains('PIM')").click()
+        cy.get("button[class='oxd-button oxd-button--medium oxd-button--secondary']").click()
+        cy.get("input[placeholder='First Name']").type('user')
+        cy.get("input[placeholder='Middle Name']").type('created')
+        cy.get("input[placeholder='Last Name']").type('uzi')
+        cy.get("button[type='submit']").click()
+        cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > form:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)").type('996633')
+        cy.get("div[class='oxd-select-text oxd-select-text--focus'] div[class='oxd-select-text-input']").type('Bangladesh').click()
+
+
+
     })
 })
