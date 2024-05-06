2b66eb34d3c79e58ed077e1c49ad502ee5d0ca61
Uzaiz Sumit <uzaiz.sumit17@gmail.com>
2024-05-06 12:35:32 +0600

    push two suites

--- cypress/e2e/uzi-custom/practice.cy.js
+++ cypress/e2e/uzi-custom/practice.cy.js
@@ -1,15 +1,16 @@
 
 
+
 describe('MyFirstTest', () => {
     it('LoginCreateUpdate', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get("input[placeholder = 'Username']").type('Admin')
-        cy.get("input[placeholder = 'Password']").type('admin123')
+        cy.get("input[placeholder = 'username']").type('Admin')
+        cy.get("input[placeholder = 'password']").type('admin123')
         cy.get("button[type = 'submit']").click()
         cy.get("ul[class*='oxd-main-menu'] li a:contains('PIM')").click()
         cy.get("button[class='oxd-button oxd-button--medium oxd-button--secondary']").click()
-        cy.get("input[placeholder='First Name']").type('user')
-        cy.get("input[placeholder='Middle Name']").type('created')
+        cy.get("input[placeholder='First name']").type('user')
+        cy.get("input[placeholder='Middle name']").type('created')
         cy.get("input[placeholder='Last Name']").type('uzi')
         cy.get("button[type='submit']").click()
         cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > form:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)").type('996633')
@@ -26,10 +27,13 @@ describe('MyFirstTest', () => {
 
     })
 
+})
+
+describe('MySecondTest', () => {
     it('Search', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get("input[placeholder = 'Username']").type('Admin')
-        cy.get("input[placeholder = 'Password']").type('admin123')
+        cy.get("input[placeholder = 'username']").type('Admin')
+        cy.get("input[placeholder = 'password']").type('admin123')
         cy.get("button[type = 'submit']").click()
         cy.get("ul[class*='oxd-main-menu'] li a:contains('PIM')").click()
         cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)").type("uzi")
